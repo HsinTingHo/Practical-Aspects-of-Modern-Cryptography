128a5a853af6117ab027570078c4af15cbc8887c
repl.it user <replituser@example.com>
2020-09-22 20:20:03 +0000

    add block_transposition_encoding

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from traditional_cryptography import ciphers as ac
+from traditional_cryptography import ciphers as c
 char_table = {
   'A':0, 'B':1, 'C':2, 'D':3, 'E':4,
   'F':5, 'G':6, 'H':7, 'I':8 ,'J':9,
@@ -11,11 +11,16 @@ test = 'STOPPOLLUTION'
 length = len(test)
 for i in range(length):
   num = char_table[test[i]]
-  print(ac.affine_encoding(17,22,num,26))
+  print(c.affine_encoding(17,22,num,26))
 
 #test caesar_decoding
 test = 'EOXHMHDQV'
 length = len(test)
 for i in range(length):
   num = char_table[test[i]]
-  print(ac.caesar_decoding(num,26))
+  print(c.caesar_decoding(num,26))
+
+#test block transposition cipher
+test = 'GRIZZLYBEARS'
+permutation_list = [3,5,1,2,4]
+print(c.block_transposition_encoding(permutation_list, test, 5))
