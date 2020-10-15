1fc57f60cb86d524febf1c927fd1c0bd7caeced3
HsinTingHo <yama762682@gmail.com>
2020-10-14 21:10:32 -0400

    added and tested decrypt

--- Assignemnt2 DES/DES.py
+++ Assignemnt2 DES/DES.py
@@ -8,7 +8,7 @@ PC2_table = tables.PC2_table
 E_table = tables.E_table
 S_table = tables.S_tables
 P_table = tables.P_table
-
+FP_table = tables.FP_table
 def hex_to_bin(msg):
     return bin(int(msg,16))[2:].zfill(64)
 
@@ -20,46 +20,53 @@ def permute(table, src_width, src):
         result[i] = src[location-1] #table start from 1
     return ''.join(result)
 
-def initial_permutation(table, src_width, src):
-    result = permute(table, src_width, src)
-    return result[:32], result[32:]
 
-def PC1(table, src_width, src):
-    result = permute(table, src_width, src)
-    return result[:28], result[28:]
-
-def PC2(table, src_width, src):
-    result = permute(table, src_width, src)
-    return result
-
-def shifting_rules():
+def shifting_rules_l():
     shift_nums = [2]*16
     one_bit_round = [1,2,9,16]
     for elem in one_bit_round:
         shift_nums[elem-1] = 1
     return shift_nums
 
-def rotate(shift_num, direction, key):
-    if direction == 'left':
-        key = key[shift_num:]+key[:shift_num]
-    else:
-        key = key[-shift_num:]+key[:-shift_num]
+def shifting_rules_r():
+    shift_nums = [26]*16
+    one_bit_round = [2,9,16]
+    shift_nums[0]=28
+    for elem in one_bit_round:
+        shift_nums[elem-1] = 27
+    return shift_nums
+
+
+
+def rotate(shift_num, key):
+    #if direction == 'left':
+    print('shift_num', shift_num)
+    key = key[shift_num:]+key[:shift_num]
+    print('rotate - key', key)
+    #else:
+        #key = key[-shift_num:]+key[:-shift_num]
     return key
 
-def transformation(table, shift_num, direction, c, d):
-    c1 = rotate(shift_num, direction, c)
-    d1 = rotate(shift_num, direction, d)
+def transformation(table, i, direction, c, d):#pass in PC2 table
+    if direction == 'left':
+        shift_nums = shifting_rules_l()
+    else:
+        shift_nums = shifting_rules_r()
+    c1 = rotate(shift_nums[i],c)
+    d1 = rotate(shift_nums[i],d)
     #print('cd1', c1+d1)
-    return PC2(table, len(table), c1+d1),c1,d1 #48 bits, 28 bits, 28 bits
+    return permute(table, len(table), c1+d1),c1,d1 #48 bits, 28 bits, 28 bits
 
 def expansion(table, src_width, src):
     return permute(table, src_width, src)
 
 def f(r_msg, key):
     expanded_r = expansion(E_table, len(E_table), r_msg) #r is expanded from 32 bits to 48 bits
+    print('expended_r', expanded_r)
     bin_r_msg = int(expanded_r, 2)
     bin_key = int(key, 2)
     xor = bin(bin_r_msg^bin_key)[2:].zfill(48)
+    print('xor result', xor)
     #divide into 8 segment and pass each segment to s_box according to order
     after_s = ''
 
@@ -78,14 +85,136 @@ def s_box(table, six_b_key):
 # key: 0123456789ABCDEF
 # plain text: 789AB123456789AA
 # cipher text: e7ab74f51fb2e8a4
-def DES(msg, key):
+
+# key: 789ABCDEF0123456
+# plain text: 45AB1A89678923A7
+# cipher text: 64b8696dfcbdda2f
+def DES_encrypt(msg, key):
+    try:
+        msg = int(msg, 16)
+        bi_msg = bin(msg)[2:].zfill(64)
+        print('msg', bi_msg)
+    except:
+        print('Plain text has to be a valid hex')
+
+    try:
+        key = int(key, 16)
+        bi_key = bin(key)[2:].zfill(64)
+        print('key', bi_key)
+    except:
+        print('Key has to be a valid hex')
+
+
 
     # Shrink key from 64 bits to 48 bits
+    pre_trans_key = permute(PC1_table, len(PC1_table), bi_key)
+    ci = pre_trans_key[:28]
+    di = pre_trans_key[28:]
+
     # perform initial permutation on msg and split msg into left half and right half
+    init_permute_msg = permute(IP_table, len(IP_table), bi_msg)
+    msg_l = init_permute_msg[:32]
+    msg_r = init_permute_msg[32:]
+
     # perform transformation on key and encrypt for 16 times
-    #     transformation 56 bits key to 48 bits
-    #     perform f on 48 bits key and right half of msg
-    #     new_right = XOR the result with left half of msg
-    #     new_left = right half
+    for i in range(16):
+        print('***** Round ',i+1,'*****')
+        #transformation 56 bits key to 48 bits
+        round_key, ci, di = transformation(PC2_table, i, 'left',ci, di)
+
+        #perform f on 48 bits key and right half of msg
+        f_out = f(msg_r, round_key)
+        bin_f_out = int(f_out,2)
+        bin_msg_l = int(msg_l,2)
+        print('msg_r', msg_r)
+        #new_left = right half
+        msg_l = msg_r
+
+        #new_right = XOR the result with left half of msg
+        msg_r = bin(bin_f_out^bin_msg_l)[2:].zfill(32)
+
+        #output for validation
+        print('Round key', round_key)
+        print('c',i,ci)
+        print('d',i,di)
+        print('f result', f_out)
+        print('New right', msg_r)
+        print('New left', msg_l)
+
     # final permutation
-    pass
+    cipher = hex(int(permute(FP_table, len(FP_table), msg_r+msg_l ),2))
+    return cipher[2:].rstrip("L")
+
+
+def DES_decrypt(cipher, key):
+    try:
+        cipher = int(cipher, 16)
+        bi_cipher = bin(cipher)[2:].zfill(64)
+        print('cipher', bi_cipher)
+    except:
+        print('Plain text has to be a valid hex')
+
+    try:
+        key = int(key, 16)
+        bi_key = bin(key)[2:].zfill(64)
+        print('key', bi_key)
+    except:
+        print('Key has to be a valid hex')
+
+    shift_nums = shifting_rules_r()
+    print('shift_nums', shift_nums)
+    # Shrink key from 64 bits to 48 bits
+    pre_trans_key = permute(PC1_table, len(PC1_table), bi_key)
+    ci = pre_trans_key[:28]
+    di = pre_trans_key[28:]
+
+    # perform initial permutation on msg and split msg into left half and right half
+    init_permute_cipher = permute(IP_table, len(IP_table), bi_cipher)
+    cipher_l = init_permute_cipher[:32]
+    cipher_r = init_permute_cipher[32:]
+
+    # perform transformation on key and encrypt for 16 times
+    for i in range(16):
+        print('***** Round ',i+1,'*****')
+        #transformation 56 bits key to 48 bits
+
+        round_key, ci, di = transformation(PC2_table, i, 'right',ci, di)
+        print('Round key', round_key)
+        #perform f on 48 bits key and right half of msg
+        f_out = f(cipher_r, round_key)
+        bin_f_out = int(f_out,2)
+        bin_cipher_l = int(cipher_l,2)
+        print('cipher_r', cipher_r)
+        #new_left = right half
+        cipher_l = cipher_r
+
+        #new_right = XOR the result with left half of msg
+        cipher_r = bin(bin_f_out^bin_cipher_l)[2:].zfill(32)
+
+        #output for validation
+
+        print('c',i,ci)
+        print('d',i,di)
+        print('f result', f_out)
+        print('New right', cipher_r)
+        print('New left', cipher_l)
+
+    # final permutation
+    msg = hex(int(permute(FP_table, len(FP_table), cipher_r+cipher_l ),2))
+    return msg[2:].rstrip("L")
+
+command = input('To encrypt a message, enter 1; to decrypt a message, enter 0:')
+if command == '1':
+    key = input('Please enter your key in hex(16 digits):')
+    msg = input('Please enter your message in hex(16 digits):')
+    result = DES_encrypt(msg, key)
+    print('\n\n============= Result =============')
+    print(result)
+elif command == '0':
+    key = input('Please enter your key in hex(16 digits):')
+    cipher = input('Please enter your cipher in hex(16 digits):')
+    result = DES_decrypt(cipher, key)
+    print('\n\n============= Result =============')
+    print(result)
+else:
+    print('Invalid command.')
--- Assignemnt2 DES/test.py
+++ Assignemnt2 DES/test.py
@@ -21,45 +21,34 @@ def test_permute():
     assert DES.permute(table, src_width, src) == '10100', 'permute failed'
 
 
-def test_initial_permutation():
-    input_str = DES.hex_to_bin('789AB123456789AA')
-    left, right = DES.initial_permutation(IP_table, len(IP_table), input_str)
-    expect = '0011000100000111001100000111110011000110101011011100001110101010'
-    assert left == expect[:32], 'initial_permutation failed'
-    assert right == expect[32:], 'initial_permutation failed'
+# def test_initial_permutation():
+#     input_str = DES.hex_to_bin('789AB123456789AA')
+#     left, right = DES.initial_permutation(IP_table, len(IP_table), input_str)
+#     expect = '0011000100000111001100000111110011000110101011011100001110101010'
+#     assert left == expect[:32], 'initial_permutation failed'
+#     assert right == expect[32:], 'initial_permutation failed'
 
-def test_shifting_rules():
+def test_shifting_rules_l():
     expect = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
-    assert DES.shifting_rules()== expect, 'shifting_rules failed'
+    assert DES.shifting_rules_l()== expect, 'shifting_rules failed'
 
 def test_rotate():
     shift_nums = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
     whole_key = '11110000110011001010101000001010101011001100111100000000'
     left_key = whole_key[:28]
     right_key = whole_key[28:]
+    print('left', left_key)
     #round 1 Rotation in encryption (left shift by 1)
-    result = DES.rotate(shift_nums[0], 'left', left_key)
+    result = DES.rotate(shift_nums[0], left_key)
     expect = '1110000110011001010101000001'
     assert expect == result, 'encryption - round1 rotation failed'
 
     #round 3 Rotation in encryption(left shift by 2)
-    result = DES.rotate(shift_nums[2], 'left', left_key)
+    result = DES.rotate(shift_nums[2], left_key)
     expect = '1100001100110010101010000011'
     assert expect == result, 'encryption - round3 rotation failed'
 
-def test_PC1():
-    input_str = DES.hex_to_bin('0123456789ABCDEF')
-    expect = '11110000110011001010101000001010101011001100111100000000'
-    left, right = DES.PC1(PC1_table, len(PC1_table), input_str)
-    assert left == expect[:28], 'PC1 failed'
-    assert right == expect[28:], 'PC1 failed'
 
-def test_PC2():
-    c1 = '1110000110011001010101000001'
-    d1 = '0101010110011001111000000001'
-    expect = '000010110000001001100111100110110100100110100101'
-    result = DES.PC2(PC2_table, len(PC2_table), c1+d1)
-    assert expect == result, 'PC2 failed'
 
 def test_transformation():
     #round1
@@ -69,8 +58,8 @@ def test_transformation():
     expect = '000010110000001001100111100110110100100110100101'
     result, c1, d1 = DES.transformation(PC2_table, rd, 'left', c, d)
     assert expect == result, 'transformation PC2 failed'
-    assert DES.rotate(rd, 'left', c) == c1, 'transformation rotation failed'
-    assert DES.rotate(rd, 'left', d) == d1, 'transformation rotation failed'
+    assert DES.rotate(rd, c) == c1, 'transformation rotation failed'
+    assert DES.rotate(rd, d) == d1, 'transformation rotation failed'
 
 def test_expansion():
     msg = '11000110101011011100001110101010'
@@ -91,19 +80,48 @@ def test_f():
     expect = '01110011101010000000010001011110'
     assert expect == result, 'f failed'
 
+def test_DES_encrypt():
+    key = '0123456789ABCDEF'
+    msg = '789AB123456789AA'
+    expect = 'e7ab74f51fb2e8a4'
+    result = DES.DES_encrypt(msg, key)
+    assert expect == result, 'DES encrypt failed'
+
+    key = '789ABCDEF0123456'
+    msg = '45AB1A89678923A7'
+    expect = '64b8696dfcbdda2f'
+    result = DES.DES_encrypt(msg, key)
+    assert expect == result, 'DES encrypt failed'
+
+def test_DES_decrypt():
+    key = '0123456789ABCDEF'
+    cipher = 'e7ab74f51fb2e8a4'
+    expect = '789ab123456789aa'
+    result = DES.DES_decrypt(cipher, key)
+    assert expect == result, 'DES decrypt failed'
+
+    key = '789ABCDEF0123456'
+    cipher = '64b8696dfcbdda2f'
+    expect = '45ab1a89678923a7'
+    result = DES.DES_decrypt(cipher, key)
+    assert expect == result, 'DES encrypt failed'
 #Start testing
 test_hex_to_bin()
 test_permute()
-test_initial_permutation()
-test_PC1()
-test_shifting_rules()
+test_shifting_rules_l()
 test_rotate()
-test_PC2()
 test_transformation()
 test_expansion()
 test_s_box()
 test_f()
-
+print('==========================')
+print('==== start encryption ====')
+print('==========================')
+test_DES_encrypt()
+print('==========================')
+print('==== start decryption ====')
+print('==========================')
+test_DES_decrypt()
 # after_p = [0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0]
 #
 # after_p_str = ''.join([str(i) for i in after_p])
